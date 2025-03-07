4c1bf39eb00d4516d8c98883c91656a5337c7de1
214g1a0511 <214g1a0511@srit.ac.in>
2025-03-08 01:40:42 +0530

    added env

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,7 @@
 const mongoose =require("mongoose")
-const connection =mongoose.connect("mongodb://127.0.0.1:27017/todosDB")
+require('dotenv').config();
+
+
+
+const connection =mongoose.connect(process.env.DBURL)
 module.exports={connection}
--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,15 @@ const app = express();
 const { connection } = require("./db");
 const { todoRouter } = require("./routes/Todos.route");
 const cors = require("cors");
+require('dotenv').config();
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/", todoRouter);
 
-app.listen("8080", async () => {
+app.listen(process.env.PORT, async () => {
   try {
     await connection;
     console.log("connected to db");
